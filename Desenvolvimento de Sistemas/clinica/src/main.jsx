9745b786bc33e87a41cee3be0d6d52547992006f
MATHEUS DE VASCONCELOS VELOZO <matheus_velozo@estudante.sesisenai.org.br>
2026-04-07 21:55:13 -0300

    Inicio do projeto de front-end da clinica

---
+++ Desenvolvimento de Sistemas/clinica/src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+//react router
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+//toastify
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+import Login from './pages/Login/Index';
+import Dashboard from './pages/Dashboard/Index';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ToastContainer />
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
